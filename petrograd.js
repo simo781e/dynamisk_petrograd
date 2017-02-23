96fb63312c9279143e38890ef000fb8e6499d855
Simon Packness <simonpackness@desktop-n2r5ao4.kea.local>
2017-02-23 13:13:03 +0100

    navn pris and rabat inserted data

--- petrograd.js
+++ petrograd.js
@@ -7,7 +7,6 @@ function sidenVises() {
     $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
 
 
-    visProdukt();
 }
 
 function visProduktListe(listen) {
@@ -20,6 +19,11 @@ function visProdukt(produkt) {
     //klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
     // indsæt data i klon
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+    var rabatpris = Math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
+    klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
 
     // append klon til .produkt_liste
     document.querySelector(".produktliste").appendChild(klon);
