c5f042672df109d9ef3384bf9f65d1772e0c761e
Simon Packness <simonpackness@desktop-n2r5ao4.kea.local>
2017-02-23 13:02:16 +0100

    load product list and create template clone

--- petrograd.js
+++ petrograd.js
@@ -2,10 +2,21 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
+
+
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
+
     visProdukt();
 }
 
-function visProdukt() {
+function visProduktListe(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
+
+function visProdukt(produkt) {
+    console.log(produkt);
     //klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
     // indsæt data i klon
